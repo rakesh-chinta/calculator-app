e0c8d4fb55a1c1cf01fd42a1f44a06ee59cd9d04
Rakesh Naga Chinta <ch.rakesh003@gmail.com>
2016-11-16 21:14:10 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,40 @@
+def add(x, y):
+    
+    return x + y
+
+def subtract(x, y):
+    
+    return x - y
+
+def multiply(x,y):
+    
+    return x * y
+
+def divide(x, y):
+    
+    return x / y
+
+print("1 for addition,2 for subtraction,3 for multiplication,4 for division")
+
+choice=input("enter your choice(1/2/3/4): ")
+           
+num1=int(input("enter the first number:  "))
+num2=int(input("enter the second number:  "))
+
+
+           
+if choice == '1' :
+           print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2' :
+           print(num1,"-",num2,"=",subtract(num1,num2))
+elif choice == '3' :
+           print(num1,"*",num2,"=",multiply(num1,num2))
+
+elif choice == '4' :
+           print(num1,"/",num2,"=",divide(num1,num2))
+
+else:
+    print("invalid input")
+           
+          
